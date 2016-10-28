a48e4c9dcff1a6a56ac6233a18a1f26329834d5f
tpadley <tylerp@dev6.com>
2016-10-28 12:57:44 -0400

    added second exercise

--- exercise/completed/getting-started/src/app/app.component.ts
+++ exercise/completed/getting-started/src/app/app.component.ts
@@ -1,20 +1,6 @@
 import {Component} from "@angular/core";
-import {Cat} from "./cat";
 @Component({
 	selector: "herding-cats",
-	template: require("./app.component.html")
+	template: `<h1>Herding Cats</h1>`
 })
-export class AppComponent {
-	boots: Cat = new Cat("Boots", "Calico", "purrs like a kitten");
-	mittens: Cat = new Cat("Mittens", "Manx", "no-tailed");
-	
-	selectedCat: Cat = this.boots;
-	
-	constructor() {
-		
-	}
-	
-	selectCat(cat: Cat): any {
-		this.selectedCat = cat;
-	}
-}
+export class AppComponent {}
--- exercise/completed/getting-started/src/app/app.module.ts
+++ exercise/completed/getting-started/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
-import {CatDetailComponent} from "./cat-detail.component";
 
 @NgModule({
 	imports: [BrowserModule],
-	declarations: [AppComponent, CatDetailComponent],
+	declarations: [AppComponent],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
