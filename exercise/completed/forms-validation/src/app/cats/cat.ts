6267a235af080b93af58b0515515f78b587baa38
tpadley <tylerp@dev6.com>
2016-10-31 12:54:02 -0400

    completed forms & validation exercise

---
+++ exercise/completed/forms-validation/src/app/cats/cat.ts
@@ -0,0 +1,3 @@
+export class Cat {
+	constructor(public id: number = 0, public name: string = "", public type: string = "", public description: string = "", public birthday: Date = new Date()) {}
+}
