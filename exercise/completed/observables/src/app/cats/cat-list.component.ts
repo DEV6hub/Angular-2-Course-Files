5ac357b34148eced72595437e872cb917acfacb4
tpadley <tylerp@dev6.com>
2016-10-31 21:20:32 -0400

    updated observable exercise

--- exercise/completed/observables/src/app/cats/cat-list.component.ts
+++ exercise/completed/observables/src/app/cats/cat-list.component.ts
@@ -1,33 +1,27 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import {Cat} from "./cat";
 import {CatService} from "./cat.service";
 import {Router} from "@angular/router";
-import {Observable, Subscription} from "rxjs";
+import {Observable} from "rxjs";
 @Component({
 	selector: "cat-list",
 	template: require("./cat-list.component.html")
 })
-export class CatListComponent implements OnInit {
+export class CatListComponent {
 	favouriteCat: Cat;
-	cats: Cat[] = [];
-	sub: Subscription;
+	cats: Observable<Cat[]>;
 	
 	constructor(private catService: CatService, private router: Router) {
 		
 	}
 	
 	ngOnInit (): any {
-		this.sub = this.catService.getCatList().subscribe((cat) => {
-			this.cats.push(cat);
-		});
+		this.cats = this.catService.getCatList().reduce((array, cat) => {
+			array.push(cat);
+			return array;
+		}, []);
 		
 		this.favouriteCat = this.catService.favouriteCat;
-	}
-	
-	ngOnDestroy(): any {
-		if ( this.sub ) {
-			this.sub.unsubscribe();
-		}
 	}
 	
 	selectCat(cat: Cat): any {
